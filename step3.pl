1984aa7b48a5d880d5fe7f573fb3b2ce9a1df8c1
cvermand <charlottevermandel@gmail.com>
2019-03-24 15:49:36 +0100

    Step 3 ! Goat cabbage and wolf

---
+++ step3.pl
@@ -0,0 +1,43 @@
+change(e,w).
+change(w,e).
+
+% [w, w, w, w] => nextConfig [e, e, w, w]
+move([X,X,Goat,Cabbage],wolf,[Y,Y,Goat,Cabbage]) :-
+     change(X,Y).
+move([X,Wolf,X,Cabbage],goat,[Y,Wolf,Y,Cabbage]) :-
+     change(X,Y).
+move([X,Wolf,Goat,X],cabbage,[Y,Wolf,Goat,Y]) :-
+     change(X,Y).
+move([X,Wolf,Goat,C],nothing,[Y,Wolf,Goat,C]) :-
+     change(X,Y).
+
+oneEq(X,X,_).
+oneEq(X,_,X).
+
+% In case we moved Wolf :safe([e, e, w, w])
+% In case we moved Goat :safe([e, w, e, w])
+safe([Man,Wolf,Goat,Cabbage]) :-
+    % Wolf case : oneEq(e, w, e)
+    % Goat case : oneEq(e, e, w)
+     oneEq(Man,Goat,Wolf), % AT ANY TIME GOAT AND WOLF MUST BE SEPARATED
+    % Wolf case : oneEq(e, w, w).
+    % Goat case : oneEq(e, e, w).
+     oneEq(Man,Goat,Cabbage).
+
+solution([e,e,e,e],[]).
+% 1TIME => config = [w, w, w, w], [Move1, Move2, Move3, Move4, Move5, Move6, Move7]
+solution(Config,[Move|Rest]) :-
+    % config = [w, w, w, w] Move = Move1, NextConfig = ?
+     move(Config,Move,NextConfig),
+     % move = wolf , nextConfig = [e, e, w, w]
+     safe(NextConfig),
+     solution(NextConfig,Rest).
+
+begin() :-
+    setof(
+        X,
+        (length(X, 7),
+        solution([w, w, w, w], X)),
+    X),
+    write(X).
+   
