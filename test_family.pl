8700c324a82cdf8d8fd4618dc4c69a33e9e09da2
cvermand <charlottevermandel@gmail.com>
2019-03-24 15:52:29 +0100

    Unit testing for step1.pl which is about the family and the ancestors

---
+++ test_family.pl
@@ -0,0 +1,150 @@
+woman(anne).
+
+woman(lisa).
+
+woman(betty).
+
+woman(sylvie).
+
+woman(eve).
+
+woman(julie).
+
+woman(valerie).
+
+man(marc).
+
+man(luc).
+
+man(jean).
+
+man(jules).
+
+man(leon).
+
+man(loic).
+
+man(gerard).
+
+man(jacques).
+
+man(herve).
+
+man(paul).
+
+husband_of(marc, anne).
+
+husband_of(luc, betty).
+
+husband_of(jules, lisa).
+
+husband_of(leon, sylvie).
+
+husband_of(loic, eve).
+
+husband_of(paul, julie).
+
+wife_of(anne, marc).
+
+wife_of(betty, luc).
+
+wife_of(lisa, jules).
+
+wife_of(sylvie, leon).
+
+wife_of(eve, loic).
+
+wife_of(julie, paul).
+
+father_in_law_of(marc, lisa).
+
+father_in_law_of(marc, sylvie).
+
+father_in_law_of(luc, jules).
+
+father_in_law_of(luc, eve).
+
+father_in_law_of(leon, paul).
+
+father_in_law_of(loic, julie).
+
+mother_in_law_of(anne, lisa).
+
+mother_in_law_of(anne, sylvie).
+
+mother_in_law_of(betty, jules).
+
+mother_in_law_of(betty, eve).
+
+mother_in_law_of(sylvie, paul).
+
+mother_in_law_of(eve, julie).
+
+child_of(jean, marc).
+
+child_of(jean, anne).
+
+child_of(jules, marc).
+
+child_of(jules, anne).
+
+child_of(leon, marc).
+
+child_of(leon, anne).
+
+child_of(jacques, jules).
+
+child_of(jacques, lisa).
+
+child_of(lisa, luc).
+
+child_of(lisa, betty).
+
+child_of(loic, luc).
+
+child_of(loic, betty).
+
+child_of(gerard, luc).
+
+child_of(gerard, betty).
+
+child_of(herve, leon).
+
+child_of(herve, sylvie).
+
+child_of(julie, leon).
+
+child_of(julie, sylvie).
+
+child_of(paul, loic).
+
+child_of(paul, eve).
+
+child_of(valerie, loic).
+
+child_of(valerie, eve).
+
+ancestor_of(marc, jacques).
+
+ancestor_of(anne, jacques).
+
+ancestor_of(marc, herve).
+
+ancestor_of(anne, herve).
+
+ancestor_of(marc, julie).
+
+ancestor_of(anne, julie).
+
+ancestor_of(luc, jacques).
+
+ancestor_of(betty, jacques).
+
+ancestor_of(luc, paul).
+
+ancestor_of(betty, paul).
+
+ancestor_of(luc, valerie).
+
+ancestor_of(betty, valerie).
+
